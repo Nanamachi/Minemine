d289663365715000dfe2892c9c505d2d914b849b
Nanamachi <7machi@nanamachi.net>
2019-05-16 11:48:15 +0900

    add Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './components/App.vue';
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+})
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 });
