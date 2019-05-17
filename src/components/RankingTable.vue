9121bb8cd54cc36a8ff33c935aae67482e2f8a3c
Nanamachi <7machi@nanamachi.net>
2019-05-17 15:36:02 +0900

    add components/RankingTable: add table mock

---
+++ src/components/RankingTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>Your Score is {{score}}!</h2>
+    <h2>Score has been posted.</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      score: 'score',
+    }),
+  },
+}
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  color: #42b983;
+}
+</style>
